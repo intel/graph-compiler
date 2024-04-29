419907cfd7b6a91c8e662e082a5df9f9827e9a82
Petr Kurapov <petr.a.kurapov@intel.com>
2024-04-29 11:56:06 +0000

    Add onednn dependency build

---
+++ README.md
@@ -0,0 +1,2 @@
+# Graph Compiler
+Graph Compiler is in active development stage.
---
+++ cmake/onednn.cmake
@@ -0,0 +1,27 @@
+include_guard()
+
+set(ONEDNN_CMAKE_ARGS
+        -DDNNL_IS_MAIN_PROJECT=FALSE
+        -DDNNL_BUILD_TESTS=FALSE
+        -DDNNL_BUILD_EXAMPLES=FALSE
+)
+
+if (${GC_ONEDNN_VERSION} STREQUAL "main")
+    set(ONEDNN_TAG ${GC_ONEDNN_VERSION})
+else ()
+    set(ONEDNN_TAG v${GC_ONEDNN_VERSION})
+endif ()
+message(STATUS "oneDNN version: ${GC_ONEDNN_VERSION}")
+set(FETCHCONTENT_QUIET FALSE)
+FetchContent_Declare(
+        oneDNN
+        GIT_REPOSITORY https://github.com/oneapi-src/oneDNN.git
+        GIT_TAG ${ONEDNN_TAG}
+        CMAKE_ARGS ${ONEDNN_CMAKE_ARGS}
+        GIT_PROGRESS TRUE
+)
+
+FetchContent_Populate(oneDNN)
+FetchContent_GetProperties(oneDNN)
+message(STATUS "oneDNN source dir: ${onednn_SOURCE_DIR}")
+FetchContent_MakeAvailable(oneDNN)
