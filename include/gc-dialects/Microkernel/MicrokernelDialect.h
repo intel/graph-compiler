82ab75ba9a5c2d3d8b283f68be306d0f1bc8e9bf
Petr Kurapov <petr.a.kurapov@intel.com>
2024-05-02 16:09:14 +0200

    Add layout for the dialects (#19)
    * Add include boilerplate for onednn graph dialect
    
    * Add include boilerplate for microkernel dialect
    
    * Add include boilerplate for linalgx dialect
    
    * Add dialects implementations

---
+++ include/gc-dialects/Microkernel/MicrokernelDialect.h
@@ -0,0 +1,16 @@
+//===- MicrokernelDialect.h - microkernel dialect ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef GC_DIALECTS_MICROKERNELDIALECT_H
+#define GC_DIALECTS_MICROKERNELDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "gc-dialects/Microkernel/MicrokernelOpsDialect.h.inc"
+
+#endif // GC_DIALECTS_MICROKERNELDIALECT_H
