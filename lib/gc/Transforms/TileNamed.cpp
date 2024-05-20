6a1274f66a55f7189ea4f348f53bbd11e8ccce8e
Jialei A Wang <jialei.a.wang@intel.com>
2024-05-20 16:02:04 +0800

    license: fix license problem

--- lib/gc/Transforms/TileNamed.cpp
+++ lib/gc/Transforms/TileNamed.cpp
@@ -1,4 +1,4 @@
-//===- TileNamed.cpp - Tile Named Linalg Ops --------------------*- C++ -*-===//
+//===-- TileNamed.cpp - Tile Named Linalg Ops -------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
