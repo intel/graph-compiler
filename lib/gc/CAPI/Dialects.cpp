05eeb5a6b887d9c733b17edcbfae0a8a4c743a1a
Xu, Rui <rui.xu@intel.com>
2024-05-20 23:47:36 -0700

    fix license

--- lib/gc/CAPI/Dialects.cpp
+++ lib/gc/CAPI/Dialects.cpp
@@ -1,3 +1,11 @@
+//===-- Dialects.cpp - DESC -------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "gc-c/Dialects.h"
 #include "gc/Dialect/OneDNNGraph/OneDNNGraphDialect.h"
 #include "mlir/CAPI/Registration.h"
--- lib/gc/CAPI/Passes.cpp
+++ lib/gc/CAPI/Passes.cpp
@@ -1,3 +1,11 @@
+//===-- Passes.cpp - DESC ---------------------------------------*- C++ -*-===//
+// 
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// 
+//===----------------------------------------------------------------------===//
+
 #include "gc/Transforms/Passes.h"
 #include "mlir-c/Pass.h"
 #include "mlir/CAPI/Pass.h"
