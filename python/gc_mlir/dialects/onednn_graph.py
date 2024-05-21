4e90b53db3870e0404d70d881788e1e839b4a6ef
Xu, Rui <rui.xu@intel.com>
2024-05-21 01:07:57 -0700

    update llvm license path

--- python/gc_mlir/dialects/onednn_graph.py
+++ python/gc_mlir/dialects/onednn_graph.py
@@ -1,19 +1,10 @@
-################################################################################
-# Copyright (C) 2024 Intel Corporation
+#===-- onednn_graph.py - DESC ---------------------------------*- Python -*-===#
 # 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # 
-# http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions
-# and limitations under the License.
-# SPDX-License-Identifier: Apache-2.0
-################################################################################
+#===------------------------------------------------------------------------===#
 
 from ._onednn_graph_ops_gen import *
 from .._mlir_libs._gc_mlir.onednn_graph import *
