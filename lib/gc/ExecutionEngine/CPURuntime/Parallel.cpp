a773ea6bcbcfebc1370aa78552a703278ee502da
Mei, Yijie <yijie.mei@intel.com>
2024-05-23 16:52:29 +0800

    f

--- lib/gc/ExecutionEngine/CPURuntime/Parallel.cpp
+++ lib/gc/ExecutionEngine/CPURuntime/Parallel.cpp
@@ -1,9 +1,9 @@
-//===-- Parallel.cpp - Definitions for parallel runtime -----------*- C++ -*-=//
-//
+//===-- Parallel.cpp - parallel ---------------------------------*- C++ -*-===//
+// 
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
+// 
 //===----------------------------------------------------------------------===//
 
 #include <assert.h>
