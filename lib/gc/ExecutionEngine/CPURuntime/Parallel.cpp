f1fd0ae69fff9b17c8d7bf354e3e160a5d3be0aa
Mei, Yijie <yijie.mei@intel.com>
2024-05-23 16:51:25 +0800

    fix

--- lib/gc/ExecutionEngine/CPURuntime/Parallel.cpp
+++ lib/gc/ExecutionEngine/CPURuntime/Parallel.cpp
@@ -1,4 +1,4 @@
-//===- Parallel.cpp - Definitions for parallel runtime  -----------*- C++ -*-=//
+//===-- Parallel.cpp - Definitions for parallel runtime -----------*- C++ -*-=//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
