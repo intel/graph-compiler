04ea5d3b74aa5a70d0096d0a686bd47ec054956b
Xu, Rui <rui.xu@intel.com>
2024-05-27 07:55:57 -0700

    fix license

--- python/gc_mlir/dialects/cpuruntime.py
+++ python/gc_mlir/dialects/cpuruntime.py
@@ -1,2 +1,5 @@
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 from ._cpuruntime_ops_gen import *
 from .._mlir_libs._gc_mlir.cpuruntime import *
