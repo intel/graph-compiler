20f62f05561edd818e9216499616b4e7cb1ee19d
Xu, Rui <rui.xu@intel.com>
2024-05-27 07:47:30 -0700

    fix license

--- python/gc_mlir/dialects/__init__.py
+++ python/gc_mlir/dialects/__init__.py
@@ -1,7 +1,3 @@
-#===-- __init__.py - DESC -------------------------------------*- Python -*-===#
-# 
-# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-# See https://llvm.org/LICENSE.txt for license information.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-# 
-#===------------------------------------------------------------------------===#
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
