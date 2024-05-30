4acf417d87d1f40a9143ee9e4f1e2bb54cf2c3db
Huang, Haixin <haixin.huang@intel.com>
2024-05-29 23:19:50 -0700

    add dialect lowering pass

--- src/gc-opt/gc-opt.cpp
+++ src/gc-opt/gc-opt.cpp
@@ -21,6 +21,7 @@
 #include "gc/Dialect/Linalgx/LinalgxDialect.h"
 #include "gc/Dialect/Microkernel/MicrokernelDialect.h"
 #include "gc/Dialect/OneDNNGraph/OneDNNGraphDialect.h"
+#include "gc/Transforms/Microkernel/MicrokernelPasses.h"
 #include "gc/Transforms/Passes.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
@@ -49,6 +50,8 @@ int main(int argc, char *argv[]) {
   mlir::gc::registerCPUPipeline();
   mlir::gc::registerGraphCompilerPasses();
   mlir::cpuruntime::registerCPURuntimePasses();
+  mlir::microkernel::registerMicrokernelPasses();
+
   mlir::DialectRegistry registry;
   registry.insert<mlir::onednn_graph::OneDNNGraphDialect>();
   registry.insert<mlir::cpuruntime::CPURuntimeDialect>();
