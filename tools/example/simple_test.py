f0916fca5de1f763bf26804b3fbf44404e6d32ec
Xu, Rui <rui.xu@intel.com>
2024-06-03 01:58:26 -0700

    fix

--- tools/example/simple_test.py
+++ tools/example/simple_test.py
@@ -26,29 +26,30 @@ project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_dir not in sys.path:
     sys.path.insert(0, project_dir)
 
+import ml_dtypes
 from bench import py_timeit_bench
 from enhanced_np_to_memref import ranked_memref_to_numpy
-from utils import get_mlir_args, get_kernel_func_from_module
+from utils import get_mlir_args
 
 if __name__ == "__main__":
     with ir.Context() as ctx:
         module = ir.Module.parse(
             """
-                func.func @main_entry(%arg0: tensor<10x10xf32>, %arg1: tensor<10x10xf32>) -> tensor<10x10xf32> attributes {llvm.emit_c_interface} {
-        %0 = tensor.empty() : tensor<10x10xf32>
-        %cst = arith.constant 0.000000e+00 : f32
-        %1 = linalg.fill ins(%cst : f32) outs(%0 : tensor<10x10xf32>) -> tensor<10x10xf32>
-        %2 = linalg.matmul ins(%arg0, %arg1 : tensor<10x10xf32>, tensor<10x10xf32>) outs(%1 : tensor<10x10xf32>) -> tensor<10x10xf32> 
-        return %2 : tensor<10x10xf32>
-    }
-                """
+            module {
+                func.func @main_entry(%arg0:tensor<10x10xbf16>, %arg1:tensor<10x10xbf16>, %arg2:tensor<10xbf16>) -> tensor<10x10xbf16> attributes {llvm.emit_c_interface} {
+                    %0 = onednn_graph.matmul %arg0, %arg1, %arg2 : (tensor<10x10xbf16>, tensor<10x10xbf16>, tensor<10xbf16>) -> tensor<10x10xbf16>
+                    return %0:tensor<10x10xbf16>
+                    }
+                }      
+            """
         )
-        arg0 = np.ones((10, 10), dtype=np.float32)
-        arg1 = np.ones((10, 10), dtype=np.float32)
+        arg0 = np.ones((10, 10), dtype=ml_dtypes.bfloat16)
+        arg1 = np.ones((10, 10), dtype=ml_dtypes.bfloat16)
+        arg2 = np.zeros((10), dtype=ml_dtypes.bfloat16)
         
     
         entry = "main_entry"
-        mlir_args = get_mlir_args(module, entry, [arg0, arg1])
+        mlir_args = get_mlir_args(module, entry, [arg0, arg1, arg2])
         passes = "any(gc-cpu-pipeline)"
         cost = py_timeit_bench(
             ctx,
@@ -62,4 +63,5 @@ if __name__ == "__main__":
         # get result
         print("cost=", cost)
         gc_res = ranked_memref_to_numpy(mlir_args[0][0])
+        print(gc_res)
         assert_allclose(gc_res, 10.0, rtol=1e-5, atol=0)
