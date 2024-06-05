62f7bee3919540f9f236a1c27ca60cd259b6462c
Longsheng Du <longsheng.du@intel.com>
2024-06-05 16:51:32 +0800

    fix

--- include/gc/Dialect/OneDNNGraph/Utils/Utils.h
+++ include/gc/Dialect/OneDNNGraph/Utils/Utils.h
@@ -1,4 +1,4 @@
-//===-- Utils.h - Utilities to support the oneDnnGraph dialect-*- C++ ---*-===//
+//===-- Utils.h - Utilities to support oneDnnGraph --------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
