0a91538d307060d636c78f22e85d0d4bbcea23d9
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-06-06 13:03:12 +0000

    Add numpy

--- .github/dockerfiles/runner/build.sh
+++ .github/dockerfiles/runner/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REGISTRY=localhost:5000
-TAG=graph-compiler-runner:latest
+TAG=graph-compiler-runner:0.0.1
 
 kubectl -n docker-registry port-forward svc/docker-registry 5000:5000 &
 
