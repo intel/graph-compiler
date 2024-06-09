5e078b08de12c39b57d9ed045018cd7003c49cdb
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2024-06-09 21:50:49 +0200

    FEAT-#128: oneDNN: Implement compilation and execution

--- test/dnnl/TestApiBasic.cpp
+++ test/dnnl/TestApiBasic.cpp
@@ -1,20 +1,10 @@
-/*
- * Copyright (C) 2024 Intel Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+//===-- TestApiBasic.cpp - Basic tests for the dnnl interface ---*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
 #include <gtest/gtest.h>
 
@@ -22,8 +12,8 @@
 #include "gc_version.h"
 #include "graph/backend/elyzor/include/dnnl_graph_compiler.h"
 
-TEST(TestDnnlCInterface, basicWorkflow) {
-  auto json = read_str_resource("mpl.json");
+TEST(TestApiBasic, basicWorkflow) {
+  auto json = read_str_resource("add.json");
 
   const struct dnnl_graph_compiler_context ctx = {.num_threads = 4};
   const struct dnnl_graph_compiler *gc;
@@ -36,19 +26,28 @@ TEST(TestDnnlCInterface, basicWorkflow) {
   // Initialize inputs and outputs
   dnnl_graph_compiler_tensor inputs[2];
   dnnl_graph_compiler_tensor outputs[1];
-  uint8_t data_buf[160];
-  int64_t dims[1] = {10};
-  inputs[0] = {.id = 0, .ndims = 1, .dims = dims, .data = data_buf};
-  inputs[1] = {.id = 1, .ndims = 1, .dims = dims, .data = &data_buf[40]};
-  outputs[0] = {.id = 2, .ndims = 1, .dims = dims, .data = &data_buf[80]};
+  float arg1[128]{1.f};
+  float arg2[128];
+  float arg3[128];
+  int64_t dims[1] = {128};
+  inputs[0] = {.id = 0, .ndims = 1, .dims = dims, .data = arg1};
+  inputs[1] = {.id = 1, .ndims = 1, .dims = dims, .data = arg2};
+  outputs[0] = {.id = 2, .ndims = 1, .dims = dims, .data = arg3};
+  for (size_t i = 0; i < 128; i++) {
+    arg2[i] = i;
+  }
 
   ASSERT_EQ(dnnl_graph_compiler_execute(exe, inputs, outputs), dnnl_success);
 
   dnnl_graph_compiler_destroy_executable(exe);
   dnnl_graph_compiler_destroy(gc);
+
+  for (size_t i = 0; i < 128; i++) {
+    ASSERT_FLOAT_EQ(arg3[i], arg1[i] + arg2[i]);
+  }
 }
 
-TEST(TestDnnlCInterface, get_version) {
+TEST(TestApiBasic, get_version) {
   auto v = dnnl_graph_compiler_get_version();
 
   ASSERT_NE(v, nullptr);
