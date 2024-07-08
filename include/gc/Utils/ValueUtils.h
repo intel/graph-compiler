dbbcb3d53ece68386d35e735b3bd0fb083de37e1
Huang, Haixin <haixin.huang@intel.com>
2024-07-07 20:15:33 -0700

    fix licenses

--- include/gc/Utils/ValueUtils.h
+++ include/gc/Utils/ValueUtils.h
@@ -1,4 +1,4 @@
-//===- ValueUtils.h - Utils for handling mlir::Value -------------*-C++ -*-===//
+//===- ValueUtils.h - Utils for handling mlir::Value ------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- lib/gc/Utils/ValueUtils.cpp
+++ lib/gc/Utils/ValueUtils.cpp
@@ -1,4 +1,4 @@
-//===-- ValueUtils.cpp ---------------------------------------*- C++ -*-===//
+//===-- ValueUtils.cpp - Utils for handling Value ------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
