722ff50d3df5bb25590e4f28951e20ac48d20901
Mei, Yijie <yijie.mei@intel.com>
2024-07-10 08:25:30 +0000

    fix runner

--- cmake/imex.cmake
+++ cmake/imex.cmake
@@ -3,11 +3,12 @@ include_guard()
 get_property(IMEX_INCLUDES GLOBAL PROPERTY IMEX_INCLUDES)
 if (NOT DEFINED IMEX_INCLUDES)
     include(functions)
-    set(IMEX_CHECK_LLVM_VERSION OFF)
+    set(IMEX_CHECK_LLVM_VERSION ON)
+    set(IMEX_ENABLE_L0_RUNTIME 1)
     # TODO: Change to main https://github.com/oneapi-src/oneDNN.git when all the
     # required functionality is merged.
     gc_fetch_content(imex 496b240093b5e132b60c5ee69878300fe69be300 https://github.com/Menooker/mlir-extensions
-            CMAKE_ARGS -DMLIR_DIR=${MLIR_DIR};-DIMEX_CHECK_LLVM_VERSION=OFF
+            CMAKE_ARGS "-DMLIR_DIR=${MLIR_DIR};-DIMEX_CHECK_LLVM_VERSION=ON;-DIMEX_ENABLE_L0_RUNTIME=1"
     )
 
     set(IMEX_INCLUDES
