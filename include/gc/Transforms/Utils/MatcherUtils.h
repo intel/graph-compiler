662a8887c3eefc5c0fbe781f857aee0e96bd101c
Dmitry Chigarev <dmitry.chigarev@intel.com>
2024-07-11 16:08:09 +0000

    licenses
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- include/gc/Transforms/Utils/MatcherUtils.h
+++ include/gc/Transforms/Utils/MatcherUtils.h
@@ -1,4 +1,4 @@
-//===- MatcherUtils.h -------------------------------------------*- C++ -*-===//
+//===- MatcherUtils.h - Matcher utils ---------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- lib/gc/Transforms/GPU/LinalgToXeGPU.cpp
+++ lib/gc/Transforms/GPU/LinalgToXeGPU.cpp
@@ -1,4 +1,4 @@
-//===- LinalgToXeGPU.cpp - Linalg To XeGPU Lowering -*- C++ -*-===//
+//===- LinalgToXeGPU.cpp - Linalg To XeGPU Lowering -------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
