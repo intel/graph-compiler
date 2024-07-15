e8b22d3f209aa2dce61adb99c99ec6ff55aabdde
Jialei A Wang <jialei.a.wang@intel.com>
2024-07-15 05:29:28 +0000

    update script

--- scripts/correctness.sh
+++ scripts/correctness.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
-pip uninstall -y benchgc || true
-pip install build/test/benchgc/dist/benchgc-*.whl
 
 # need to import tools as a package 
 export PYTHONPATH=$(pwd)
 export MLIR_RUNNER_UTILS=${MLIR_DIR}/../../libmlir_runner_utils.so
 export MLIR_C_RUNNER_UTILS=${MLIR_DIR}/../../libmlir_c_runner_utils.so
-export LD_PRELOAD=/lib/x86_64-linux-gnu/libomp.so.5
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1
 
 python3 -m benchgc --verbose 0 --driver linalg --case matmul_transpose_b -i 1024x512xf32:D -i 1024x512xf32:D -o 1024x1024xf32:D --cast cast_signed
