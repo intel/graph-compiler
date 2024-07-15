becc82a4e684288324200931043d2ff8bd8904e2
Jialei A Wang <jialei.a.wang@intel.com>
2024-07-15 06:15:17 +0000

    update benchgc dependency

--- test/benchgc/setup.py
+++ test/benchgc/setup.py
@@ -26,5 +26,5 @@ setuptools.setup(
     packages=setuptools.find_packages("src")
     + setuptools.find_packages("../../python_packages/gc_mlir_core"),
     package_data={"gc_mlir": ["_mlir_libs/*.so"]},
-    install_requires=["torch", "numpy"],
+    install_requires=["torch", "numpy", "ml_dtypes"],
 )
