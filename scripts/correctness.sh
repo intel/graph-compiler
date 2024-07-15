caeeea43895ecf70343c6b2670b64edc7b1ed5f1
Jialei A Wang <jialei.a.wang@intel.com>
2024-07-15 07:59:32 +0000

    fix script

--- scripts/correctness.sh
+++ scripts/correctness.sh
@@ -3,8 +3,8 @@
 
 # need to import tools as a package 
 export PYTHONPATH=$(pwd)
-export MLIR_RUNNER_UTILS=$(pwd)/install/llvm/lib/libmlir_runner_utils.so
-export MLIR_C_RUNNER_UTILS=$(pwd)/install/llvm/lib/libmlir_c_runner_utils.so
+export MLIR_RUNNER_UTILS=/runner/_work/graph-compiler/install/llvm/lib/libmlir_runner_utils.so
+export MLIR_C_RUNNER_UTILS=/runner/_work/graph-compiler/install/llvm/lib/libmlir_c_runner_utils.so
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1
 
 python3 -m benchgc --verbose 0 --driver linalg --case matmul_transpose_b -i 1024x512xf32:D -i 1024x512xf32:D -o 1024x1024xf32:D --cast cast_signed
