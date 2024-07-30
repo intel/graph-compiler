375d491ea1f41b551535b514eb6e9139564f09fd
Alexei Fedotov <alexei.fedotov@gmail.com>
2024-07-30 23:43:37 +0200

    Support multiple LLVM builds

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -50,11 +50,9 @@ LLVM_HASH=$(cat cmake/llvm-version.txt)
 load_llvm() {
     local run_id
 
-    run_id=$(gh run list -w "LLVM Build" --repo $repo --json databaseId --jq '.[0].databaseId')
-
     gh run download "$run_id" \
         --repo "$repo" \
-        --pattern "llvm-$LLVM_HASH" \
+        -n "llvm-$LLVM_HASH" \
         --dir "$llvm_dir"
     cd "$llvm_dir"
     tar -zxf "llvm-$LLVM_HASH"/llvm.tgz
