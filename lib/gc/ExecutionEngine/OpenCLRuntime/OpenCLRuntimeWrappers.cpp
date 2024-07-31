f9d5cb7acabe39e7122250bcbdee1f4f21808308
Mei, Yijie <yijie.mei@intel.com>
2024-07-31 05:54:57 +0000

    fix cmake

--- lib/gc/ExecutionEngine/OpenCLRuntime/OpenCLRuntimeWrappers.cpp
+++ lib/gc/ExecutionEngine/OpenCLRuntime/OpenCLRuntimeWrappers.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#define CL_TARGET_OPENCL_VERSION 300
 #include <CL/cl.h>
 #include <CL/cl_ext.h>
 #include <array>
