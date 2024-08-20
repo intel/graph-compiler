ed1c5a0f338a60265dcba76d3bbe467bd509a5f3
Xu, Xiaohui1 <xiaohui1.xu@intel.com>
2024-08-20 11:04:15 +0800

    update file name

--- lib/gc/Transforms/LowerToTileVector.cpp
+++ lib/gc/Transforms/LowerToTileVector.cpp
@@ -1,4 +1,4 @@
-//===-- LowerTileVectorPass.cpp - Lower Op to vector ------------*- C++ -*-===//
+//===-- LowerToTileVector.cpp - Lower Op to vector --------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
