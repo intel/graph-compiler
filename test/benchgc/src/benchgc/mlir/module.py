b3cbfaadea72c4bc445e57a0af6f145020615755
Jialei A Wang <jialei.a.wang@intel.com>
2024-09-03 02:49:19 +0000

    support reduce op with fast implementation

--- test/benchgc/src/benchgc/mlir/module.py
+++ test/benchgc/src/benchgc/mlir/module.py
@@ -17,8 +17,9 @@
 from typing import Callable, List, Tuple
 
 from benchgc.mlir.arg import MLIRArg
+from benchgc.mlir.util import str_to_mlir_dtype
 from gc_mlir import ir
-from gc_mlir.dialects import func
+from gc_mlir.dialects import func, linalg
 
 
 def init_module(
@@ -45,4 +46,45 @@ def init_module(
             with ir.InsertionPoint(f.add_entry_block()):
                 block_args = f.entry_block.arguments
                 func.ReturnOp(op_func(ctx, *block_args))
+        return module
+
+
+def init_reduce_module(
+    entry_name: str,
+    input: MLIRArg,
+    output: MLIRArg,
+    dimensions: List[int],
+    reduce_buf: Callable[[ir.Context], ir.OpView],
+    reduce_func: Callable[[ir.BlockArgument, ir.BlockArgument], ir.OpResult],
+) -> ir.Module:
+    with ir.Context() as ctx, ir.Location.unknown():
+        module = ir.Module.create()
+        with ir.InsertionPoint(module.body):
+            f = func.FuncOp(
+                name=entry_name,
+                type=ir.FunctionType.get(
+                    inputs=[input.get_mlir_type(ctx)],
+                    results=[output.get_mlir_type(ctx)],
+                ),
+            )
+            f.attributes["llvm.emit_c_interface"] = ir.UnitAttr.get()
+
+            with ir.InsertionPoint(f.add_entry_block()):
+                block_args = f.entry_block.arguments
+
+                # op_empty = outputs[0].get_zero_op(ctx)
+                op_reduce = linalg.ReduceOp(
+                    result=[output.get_ranked_tensor_type(ctx)],
+                    inputs=[block_args[0]],
+                    inits=[reduce_buf(ctx)],
+                    dimensions=dimensions,
+                )
+                block = op_reduce.regions[0].blocks.append(
+                    str_to_mlir_dtype(ctx, input.dtype),
+                    str_to_mlir_dtype(ctx, output.dtype),
+                )
+                with ir.InsertionPoint(block):
+                    arg0, arg1 = block.arguments
+                    linalg.YieldOp([reduce_func(arg0, arg1)])
+                func.ReturnOp(op_reduce)
         return module
