23269d7882419924ff1f4290e2668468ba1525db
Ivy Zhang <yan3.zhang@intel.com>
2024-09-04 15:34:32 +0800

    [Transform] fix legalization on absi (#310)
    fix legalization on absi

--- lib/gc/Transforms/Pipeline.cpp
+++ lib/gc/Transforms/Pipeline.cpp
@@ -153,7 +153,7 @@ void populateLoweringToLLVMPasses(mlir::OpPassManager &pm) {
   pm.addPass(createConvertSCFToCFPass());
   pm.addPass(cpuruntime::createCPURuntimeToLLVM());
   pm.addPass(createConvertOpenMPToLLVMPass());
-  pm.addNestedPass<func::FuncOp>(createConvertMathToLLVMPass());
+  pm.addPass(createConvertMathToLLVMPass());
   pm.addPass(createConvertMathToLibmPass());
   pm.addNestedPass<func::FuncOp>(createArithToLLVMConversionPass());
   pm.addPass(createConvertVectorToLLVMPass());
