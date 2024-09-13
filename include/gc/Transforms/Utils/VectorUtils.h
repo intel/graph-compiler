3552eb662112c659c573a659fd7d84ed426050b1
Xu, Xiaohui1 <xiaohui1.xu@intel.com>
2024-09-13 15:25:16 +0800

    fix license

--- include/gc/Transforms/Utils/VectorUtils.h
+++ include/gc/Transforms/Utils/VectorUtils.h
@@ -1,4 +1,4 @@
-//===-- VectorUtils.h ----- vector fusion analysis --------------*- C++ -*-===//
+//===-- VectorUtils.h - vector fusion analysis ------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
