2e3e37761025c1abe8c93e5bbc2e5b4c6fb0715c
Zhang, Yifei <yifei.zhang@intel.com>
2024-09-17 21:21:58 -0700

    fix license

--- lib/gc/Transforms/PostProcessPackUnpack.cpp
+++ lib/gc/Transforms/PostProcessPackUnpack.cpp
@@ -1,4 +1,4 @@
-//===-- PostProcessPackUnpack.cpp - Fold and simplify pack unpack *- C++-*-===//
+//===-- PostProcessPackUnpack.cpp - Simplify pack unpack --------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- lib/gc/Transforms/PropagateLayout.cpp
+++ lib/gc/Transforms/PropagateLayout.cpp
@@ -1,4 +1,4 @@
-//===-- PropagateLayout.cpp - Propagate packing on named ops*- C++ ------*-===//
+//===-- PropagateLayout.cpp - Propagate packing on named ops ----*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
