1a61da03330b7c070902b0c0a0daf73f3fcdb26f
Song, Yunfei <yunfei.song@intel.com>
2024-09-18 00:49:09 -0700

    fix license

--- lib/gc/Transforms/TileUsingInterfaceX.cpp
+++ lib/gc/Transforms/TileUsingInterfaceX.cpp
@@ -1,4 +1,4 @@
-//===-- TileUsingInterfaceX.cpp -  upstream eXtension ---------*- C++ -*-===//
+//===-- TileUsingInterfaceX.cpp - upstream eXtension ------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
