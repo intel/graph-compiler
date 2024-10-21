c195da358091888999e980a1465f8d46a09f78d1
Petr Kurapov <petr.a.kurapov@intel.com>
2024-10-21 05:45:33 -0700

    [GPU] Add XeVM dialect declaration

---
+++ lib/gc/Dialect/LLVMIR/IR/XeVMDialect.cpp
@@ -0,0 +1,36 @@
+//===-- XeVMDialect.cpp - XeVM dialect registration -------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#include "gc/Dialect/LLVMIR/XeVMDialect.h"
+
+#include "mlir/Dialect/GPU/IR/CompilationInterfaces.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+using namespace mlir;
+using namespace xevm;
+
+#include "gc/Dialect/LLVMIR/XeVMOpsDialect.cpp.inc"
+
+void XeVMDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "gc/Dialect/LLVMIR/XeVMOps.cpp.inc"
+      >();
+
+  addAttributes<
+#define GET_ATTRDEF_LIST
+#include "gc/Dialect/LLVMIR/XeVMOpsAttributes.cpp.inc"
+      >();
+}
+
+#define GET_OP_CLASSES
+#include "gc/Dialect/LLVMIR/XeVMOps.cpp.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "gc/Dialect/LLVMIR/XeVMOpsAttributes.cpp.inc"
