0e76a49b236b149514fffee7bc2dac0aa52b6b2a
dchigarev <dmitry.chigarev@intel.com>
2026-06-26 13:02:56 +0000

    [Lit] Do not pass 'execute_external=True' to ShTest constructor
    Signed-off-by: dchigarev <dmitry.chigarev@intel.com>

--- test/mlir/lit.cfg.py
+++ test/mlir/lit.cfg.py
@@ -14,7 +14,7 @@ from lit.llvm import llvm_config
 # name: The name of this test suite.
 config.name = "GRAPH_COMPILER_OPT"
 
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.test_format = lit.formats.ShTest()
 
 # suffixes: A list of file extensions to treat as test files.
 config.suffixes = [".mlir"]
@@ -30,8 +30,8 @@ config.substitutions.append(("%PATH%", config.environment["PATH"]))
 config.substitutions.append(("%shlibext", config.llvm_shlib_ext))
 config.substitutions.append(("%llvmlibdir", config.llvm_lib_dir))
 config.substitutions.append(("%gclibdir", config.gc_obj_root + "/lib/"))
-config.substitutions.append(('%mlir_runner_utils', config.mlir_runner_utils))
-config.substitutions.append(('%mlir_c_runner_utils', config.mlir_c_runner_utils))
+config.substitutions.append(("%mlir_runner_utils", config.mlir_runner_utils))
+config.substitutions.append(("%mlir_c_runner_utils", config.mlir_c_runner_utils))
 
 llvm_config.with_system_environment(["HOME", "INCLUDE", "LIB", "TMP", "TEMP"])
 
